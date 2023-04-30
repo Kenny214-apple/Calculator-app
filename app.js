d467013aba02bf13e61dcf626045b57509a69ba4
Kenny214-apple <kendumoimoi@gmail.com>
2023-04-30 12:58:56 +0100

    improve overall arithmetic performance

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// DOM manipulation
 const allBtn = document.querySelectorAll(".botton");
 const render = document.querySelector(".top-area");
 const reset = document.querySelector(".botton-reset");
@@ -5,75 +6,100 @@ const resultBtn = document.querySelector(".button-equal");
 const delBtn = document.querySelector(".botton-del");
 const athBtn = document.querySelectorAll(".arithmetic");
 
+// setting default Arithemetics key values
 let addClicked = false;
 let subClicked = false;
 let divClicked = false;
 let mulClicked = false;
 let data = [];
 
+// Event function for Number buttons
 allBtn.forEach(function (val) {
   val.addEventListener("click", calculate);
 });
 
+// calculation function
 function calculate(event) {
-  var target = event.target.id;
+  var target = event.target.innerHTML;
   render.textContent += target;
-  console.log(render);
-  console.log(render.innerHTML);
 }
 
+// Event function for Arth keys
 athBtn.forEach(function (val) {
   val.addEventListener("click", arithmetic);
 });
 
-function arithmetic(para) {
-  var getTo = para.target.id;
-  console.log(typeof getTo);
+// Reseting bolean values of arths keys
+function arithmetic(event) {
+  var getTo = event.target.innerHTML;
   if (getTo === "+") {
     addClicked = true;
+    subClicked = false;
+    mulClicked = false;
+    divClicked = false;
   } else if (getTo === "-") {
     subClicked = true;
+    addClicked = false;
+    mulClicked = false;
+    divClicked = false;
   } else if (getTo === "/") {
     divClicked = true;
+    subClicked = false;
+    mulClicked = false;
+    addClicked = false;
   } else if (getTo === "*") {
     mulClicked = true;
+    subClicked = false;
+    addClicked = false;
+    divClicked = false;
   }
-  var output = parseInt(render.innerHTML);
+  var output = parseFloat(render.innerHTML);
   data.push(output);
-  console.log(data);
   render.textContent = "";
 }
 
+// Event function for reault btn
 resultBtn.addEventListener("click", result);
 function result() {
   let addRender = 0;
-  let mulRender = 1;
-  let divRender = 0;
-  let subRender = 0;
+  // let divRender = 0;
+  console.log(data);
 
-  var output = parseInt(render.innerHTML);
+  var output = parseFloat(render.innerHTML);
   data.push(output);
   console.log(data);
 
+  // Conditional statement for addition function
   if (addClicked == true) {
     for (let index = 0; index < data.length; index++) {
       addRender += data[index];
     }
     render.textContent = addRender;
+    data = [];
+    addClicked = false;
+
+    //  Conditional statement for subtraction function
   } else if (subClicked == true) {
-    for (let index = 0; index < data.length; index++) {
-      subRender += data[0] - data[1];
-    }
+    let subRender = data.reduce((a, b) => a - b);
     render.textContent = subRender;
-  } else if (divClicked == true) {
-    divRender += data[0] / data[1];
-    let newRender = divRender.toFixed(7);
-    render.textContent = newRender;
-  } else if (mulClicked == true) {
-    for (let index = 0; index < data.length; index++) {
-      mulRender *= data[index];
-    }
+    data = [];
+    subClicked = false;
+  }
+  //  Conditional statement for division function
+  else if (divClicked == true) {
+    let divRender = data.reduce((a, b) => a / b);
+    // let newRender = divRender.toFixed(10);
+    render.textContent = divRender;
+    data = [];
+    divClicked = false;
+  }
+
+  //  Conditional statement for multiplication function
+  else {
+    let mulRender = data.reduce((a, b) => a * b);
     render.textContent = mulRender;
+    data = [];
+    mulClicked = false;
   }
 }
 
